d058ec883867a38c7317921e6f4c528a02d65380
DimaVnuk <vnuchok311994@hotmail.com>
2022-03-21 17:58:23 +0300

    init toast library

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ButtonProps {
+  children: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.children}</button>;
+}
+
+export default Button;
