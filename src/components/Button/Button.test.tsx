458aea87860e7edcf221474f24a5462fc8839f86
DimaVnuk <vnuchok311994@hotmail.com>
2022-03-28 16:53:38 +0300

    refactor: deleted test Button

--- src/components/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Button from "./Button";
-
-describe("Button", () => {
-  test("render the Button component", () => {
-    render(<Button children="Test Button" />);
-  });
-});
