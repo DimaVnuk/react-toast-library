a9685a5e1923e5705dc9fddddc1800634d002990
DimaVnuk <vnuchok311994@hotmail.com>
2022-03-29 15:50:57 +0300

    fix: changed export in enum object

--- src/container/ToastSingleton.tsx
+++ src/container/ToastSingleton.tsx
@@ -1,137 +1,124 @@
-import React from 'react';
-import { Toast } from '../components/Toast/Toast';
-import { ToastContainerPortal } from '../components/Toast/ToastContainerPortal';
-const errorIcon = require('../assets/svg/error.svg') as string;
-const infoIcon = require('../assets/svg/info.svg') as string;
-const warningIcon = require('../assets/svg/warning.svg') as string;
-const successIcon = require('../assets/svg/success.svg') as string;
+import React from "react";
+import { Toast } from "../components/Toast/Toast";
+import { ToastContainerPortal } from "../components/Toast/ToastContainerPortal";
+const errorIcon = require("../assets/svg/error.svg") as string;
+const infoIcon = require("../assets/svg/info.svg") as string;
+const warningIcon = require("../assets/svg/warning.svg") as string;
+const successIcon = require("../assets/svg/success.svg") as string;
 import {
-   WHITE,
-   GREEN,
-   RED,
-   PURPLE,
-   BLACK,
-   YELLOW,
-   GRAY,
-   SUCCESS,
-   ERROR,
-   INFO,
-   WARNING,
-   SUCCESS_TITLE,
-   WARNING_TITLE,
-   ERROR_TITLE,
-   INFO_TITLE,
-   CUSTOM_TITLE,
-} from '../constants/index';
+  Color,
+  Types,
+  Title
+} from "../constants/index";
 
 let toastList: string[] = [];
 
 class Singleton {
-   toastList: string[] | undefined;
-   private static singleton: Singleton;
-   constructor(toastList: string[]) {
-      if (Singleton.singleton) {
-         return Singleton.singleton;
-      }
-      Singleton.singleton = this;
-      this.toastList = toastList;
-   }
+  toastList: string[] | undefined;
+  private static singleton: Singleton;
+  constructor(toastList: string[]) {
+    if (Singleton.singleton) {
+      return Singleton.singleton;
+    }
+    Singleton.singleton = this;
+    this.toastList = toastList;
+  }
 
-   getId() {
-      return Math.floor(Math.random() * 101);
-   }
+  getId() {
+    return Math.floor(Math.random() * 101);
+  }
 
-   getTitleName(prop) {
-      switch (prop.type) {
-         case SUCCESS:
-            return SUCCESS_TITLE;
-         case ERROR:
-            return ERROR_TITLE;
-         case INFO:
-            return INFO_TITLE;
-         case WARNING:
-            return WARNING_TITLE;
-         default:
-            return CUSTOM_TITLE;
-      }
-   }
+  getTitleName(prop) {
+    switch (prop.type) {
+      case Types.Success:
+        return Title.SuccessTitle;
+      case Types.Error:
+        return Title.ErrorTitle;
+      case Types.Info:
+        return Title.InfoTitle;
+      case Types.Warning:
+        return Title.WarningTitle;
+      default:
+        return Title.CustomTitle;
+    }
+  }
 
-   getTitleColor(prop) {
-      switch (prop.type) {
-         case SUCCESS:
-            return WHITE;
-         case ERROR:
-            return WHITE;
-         case INFO:
-            return WHITE;
-         case WARNING:
-            return BLACK;
-         default:
-            return BLACK;
-      }
-   }
+  getTitleColor(prop) {
+    switch (prop.type) {
+      case Types.Success:
+        return Color.White;
+      case Types.Error:
+        return Color.White;
+      case Types.Info:
+        return Color.White;
+      case Types.Warning:
+        return Color.Black;
+      default:
+        return Color.Black;
+    }
+  }
 
-   getBackgroundColor(prop) {
-      switch (prop.type) {
-         case SUCCESS:
-            return GREEN;
-         case ERROR:
-            return RED;
-         case INFO:
-            return PURPLE;
-         case WARNING:
-            return YELLOW;
-         default:
-            return GRAY;
-      }
-   }
+  getBackgroundColor(prop) {
+    switch (prop.type) {
+      case Types.Success:
+        return Color.Green;
+      case Types.Error:
+        return Color.Red;
+      case Types.Info:
+        return Color.Purple;
+      case Types.Warning:
+        return Color.Yellow;
+      default:
+        return Color.Gray;
+    }
+  }
 
-   getIcon(prop) {
-      switch (prop.type) {
-         case SUCCESS:
-            return successIcon;
-         case ERROR:
-            return errorIcon;
-         case INFO:
-            return infoIcon;
-         case WARNING:
-            return warningIcon;
-         default:
-            return successIcon;
-      }
-   }
+  getIcon(prop) {
+    switch (prop.type) {
+      case Types.Success:
+        return successIcon;
+      case Types.Error:
+        return errorIcon;
+      case Types.Info:
+        return infoIcon;
+      case Types.Warning:
+        return warningIcon;
+      default:
+        return successIcon;
+    }
+  }
 
-   getProp(desc, prop) {
-      return {
-         ...prop,
-         id: prop.toastId || this.getId(),
-         description: desc || 'Description',
-         toastPadding: prop.padding || '',
-         title: this.getTitleName(prop),
-         titleColor: this.getTitleColor(prop),
-         descColor: this.getTitleColor(prop),
-         backgroundColor: this.getBackgroundColor(prop),
-         icon: this.getIcon(prop),
-      };
-   }
+  getProp(desc, prop) {
+    return {
+      ...prop,
+      id: prop.toastId || this.getId(),
+      description: desc || "Description",
+      toastPadding: prop.padding || "",
+      title: this.getTitleName(prop),
+      titleColor: this.getTitleColor(prop),
+      descColor: this.getTitleColor(prop),
+      backgroundColor: this.getBackgroundColor(prop),
+      icon: this.getIcon(prop),
+    };
+  }
 
-   showToast(desc, prop) {
-      if (toastList.length < 5) {
-         toastList = [...toastList, this.getProp(desc, prop)];
-      }
+  showToast(desc, prop) {
+    if (toastList.length < 5) {
+      toastList = [...toastList, this.getProp(desc, prop)];
+    }
 
-      return (
-         <ToastContainerPortal>
-            <Toast
-               toastList={toastList}
-               position={prop.position}
-               autoDelete={prop.autoDelete}
-               autoDeleteTime={prop.delay}
-               animation={prop.animation}
-            />
-         </ToastContainerPortal>
-      );
-   }
+    return (
+      <ToastContainerPortal>
+        <Toast
+          toastList={toastList}
+          position={prop.position}
+          autoDelete={prop.autoDelete}
+          autoDeleteTime={prop.delay}
+          animation={prop.animation}
+        />
+      </ToastContainerPortal>
+    );
+  }
 }
 
 export const toast = new Singleton(toastList);
