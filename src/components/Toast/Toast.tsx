b1931686a294f2061ba8097940f552e1b96c9342
DimaVnuk <vnuchok311994@hotmail.com>
2022-03-29 18:28:29 +0300

    refactor: added function renderToast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -36,50 +36,51 @@ export const Toast = ({
     setList([...list]);
   };
 
+  const renderToast = list.map((toast) => (
+    <div
+      key={toast.id + 1}
+      className={`notification toast ${position}`}
+      style={{
+        backgroundColor: toast.backgroundColor,
+        padding: toast.toastPadding,
+      }}
+    >
+      <button
+        onClick={() => deleteToast(toast.id)}
+        style={{
+          color: toast.titleColor,
+        }}
+      >
+        <span className="close">X</span>
+      </button>
+      <div className="notification-image">
+        <img src={toast.icon} />
+      </div>
+      <div>
+        <p
+          className="notification-title"
+          style={{
+            color: toast.titleColor,
+          }}
+        >
+          {toast.title}
+        </p>
+        <p
+          className="notification-message"
+          style={{
+            color: toast.descColor,
+          }}
+        >
+          {toast.description}
+        </p>
+      </div>
+    </div>
+  ));
   return (
     <ErrorBoundary>
       <ToastWrapper animation={animation}>
         <div className={`notification-container ${position}`}>
-          {list.map((toast) => (
-            <div
-              key={toast.id + 1}
-              className={`notification toast ${position}`}
-              style={{
-                backgroundColor: toast.backgroundColor,
-                padding: toast.toastPadding,
-              }}
-            >
-              <button
-                onClick={() => deleteToast(toast.id)}
-                style={{
-                  color: toast.titleColor,
-                }}
-              >
-                <span className="close">X</span>
-              </button>
-              <div className="notification-image">
-                <img src={toast.icon} />
-              </div>
-              <div>
-                <p
-                  className="notification-title"
-                  style={{
-                    color: toast.titleColor,
-                  }}
-                >
-                  {toast.title}
-                </p>
-                <p
-                  className="notification-message"
-                  style={{
-                    color: toast.descColor,
-                  }}
-                >
-                  {toast.description}
-                </p>
-              </div>
-            </div>
-          ))}
+          {renderToast}
         </div>
       </ToastWrapper>
     </ErrorBoundary>
